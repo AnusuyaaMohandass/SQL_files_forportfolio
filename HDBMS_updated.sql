7fa77d15637d833d1c02b03f63d6104bee2cb9cc
AnusuyaaMohandass <115528416+AnusuyaaMohandass@users.noreply.github.com>
2023-10-24 14:43:25 +0400

    Add files via upload
    Hi all, 
    This is just a small attempt from my end in creating what might look like all the details of a patient in a hospital database. A hospital database should also include details like the staff members, physicians who treat the patients etc, I have not included those tables. 
    
    My database is called the Hospital_mgmt_db which consists of 3 tables, 1 view and a stored procedure. 
    
    Tables:
    1) patient_details
    2) Patients_Health_History
    3) patients_blood_pressure_level
    View:
    1) patient_bs_bp_level
    Stored Procedure
    1) BMI_calculation
    
    - It consists of data of just 20 patients. I would like to extend the number of tables in this database and also add triggers.
    
    -'patient_details' table consists of the basic details of the patient like patient_id, patient_name, weight, height, gender, city where patient_id is the primary key.
    
    - 'Patients_Health_History' table consists of details like patient_id, blood_sugar_level, existing_medical_condition('yes','no'), EMC_name, Surgeries_History, No_of_surgeries.
    
    - Since I had forgotten to add the blood_pressure_level column in the 'Patients_Health_History', I created a new table called 'patients_blood_pressure_level' table. Initially, I did try to add the column using the Alter command. When I tried to insert the values in the 'Patients_Health_History' table, it got added after the existing table ended which means the blood pressure values were only added after 20 rows, leaving the first 20 rows as null. That's when I came up with the idea of presenting them as view.
    
    - In the View 'patient_bs_bp_level', all the details including the patient_id_patient_name, gender, height, weight, blood_sugar_level, blood_pressure_level were added. 
    
    - Finally the stored procedure 'BMI_calculation', helps us in calculating the BMI of any patient when the patient_id is given. The input parameter in this case is the patient_id. Once, the patient_id is given, the patient_name, patient_id and the BMI is calculated and displayed. 
    
    I understand that what I've created is just a primitive level of database and I'm open to all suggestions which will help me make a better database. 
    
    Thanks
    Anusuyaa

---
+++ HDBMS_updated.sql
@@ -0,0 +1,151 @@
+
+CREATE DATABASE Hospital_mgmt_db;
+CREATE TABLE IF NOT EXISTS Hospital_mgmt_db.patient_details
+(
+	patient_id int primary key not null,
+	patient_name varchar(50),
+	age int,
+    gender enum('M', 'F'),
+	weight decimal(10,2),
+	height decimal(10,2),
+	city varchar(50)
+);
+
+DROP TABLE IF EXISTS Hospital_mgmt_db.patient_details;
+CREATE TABLE Hospital_mgmt_db.Staff
+(
+	hospital_id int primary key not null,
+    first_name varchar(50),
+    last_name varchar(50),
+    age int,
+    designation varchar(50),
+    join_date date,
+    last_date date,
+    contact_ext int
+    );
+    
+CREATE TABLE Hospital_mgmt_db.Patients_Health_History
+(
+	patient_id int,
+    blood_sugar_level decimal(10,2),
+    Existing_Medical_Conditions enum('yes', 'no'),
+    EMC_name varchar(50),
+    Surgeries_History enum('yes', 'no'),
+    No_of_Surgeries int
+    );
+
+
+INSERT INTO Hospital_mgmt_db.patient_details
+(
+	patient_id,
+	patient_name,
+	age,
+    gender,
+	weight,
+	height,
+	city
+)
+VALUES(201001, 'Madhavan Kutty', 59, 'M', 60, 170, 'Chennai'),
+(201002, 'Madhavan Paresh', 46, 'M', 70, 170, 'Chennai'),
+(201003, 'Mariamma George', 52, 'F', 65, 160, 'Chennai'),
+(201004, 'Mohammed Rafi', 32, 'M', 70, 170, 'Chennai'),
+(201005, 'Paresh Rawal', 45, 'M', 72, 175, 'Delhi'),
+(201006, 'Manish Parul', 36, 'M', 70, 170, 'Mumbai'),
+(201007, 'Karishma Paresh', 29, 'F', 70, 160, 'Chennai'),
+(201008, 'Karishma Kapoor', 30, 'F', 70, 160, 'Pune'),
+(201009, 'Rutuja Paresh', 35, 'F', 70, 160, 'Indore'),
+(201010, 'Karishma Paresh', 29, 'F', 70, 160, 'Chennai'),
+(201011, 'Alia Kapoor', 34, 'F', 52, 160, 'Jaipur'),
+(201012, 'Priyamani Gupta', 37, 'F', 70, 160, 'Chennai'),
+(201013, 'Nana Patekar', 65, 'M', 70, 160, 'Delhi'),
+(201014, 'Sunny Deol', 64, 'M', 70, 160, 'Manali'),
+(201015, 'Abhay Singh', 29, 'M', 70, 160, 'Lucknow'),
+(201016, 'Happy Singh', 26, 'M', 70, 180, 'Mohali'),
+(201017, 'Diya Sharma', 19, 'F', 85, 160, 'Lucknow'),
+(201018, 'Harpreet Kaur', 26, 'F', 70, 180, 'Mohali'),
+(201019, 'Rani Singh', 30, 'F', 70, 180, 'Mohali'),
+(201020, 'Mann Singh', 26, 'M', 70, 180, 'Lucknow');
+
+Select * from Hospital_mgmt_db.Patients_Health_History;
+INSERT INTO Hospital_mgmt_db.Patients_Health_History
+VALUES(201001, 100.56, 'yes', 'diabetes', 'no', '0'), 
+(201002, 91.86, 'no', 'nil', 'no', '0'), 
+(201003, 75.74, 'yes', 'diabetes', 'no', '0'), 
+(201004, 150.65, 'no', 'nil', 'no', '0'), 
+(201005, 170.76, 'yes', 'diabetes', 'yes', '1'), 
+(201006, 95.86, 'no', 'nil', 'no', '0'), 
+(201007, 200.00, 'yes', 'diabetes', 'yes', '1'), 
+(201008, 100.56, 'yes', 'diabetes', 'no', '0'), 
+(201009, 91.86, 'no', 'nil', 'no', '0'), 
+(201010, 75.74, 'yes', 'hypertension', 'yes', '1'), 
+(201011, 150.65, 'yes', 'diabetes', 'no', '0'), 
+(201012, 170.76, 'yes', 'diabetes', 'yes', '2'), 
+(201013, 95.86, 'yes', 'hypertension', 'no', '0'), 
+(201014, 100.56, 'yes', 'diabetes', 'yes', '1'), 
+(201015, 91.86, 'no', 'nil', 'no', '0'), 
+(201016, 75.74, 'yes', 'hypertension', 'no', '0'), 
+(201017, 150.65, 'yes', 'diabetes', 'yes', '2'), 
+(201018, 170.76, 'yes', 'diabetes', 'no', '0'), 
+(201019, 95.86, 'no', 'nil', 'no', '0'), 
+(201020, 200.00, 'yes', 'diabetes', 'yes', '1');
+
+CREATE TABLE Hospital_mgmt_db.patients_blood_pressure_level
+(
+patient_id int primary key,
+blood_pressure_level varchar(50)
+);
+
+INSERT INTO Hospital_mgmt_db.patients_blood_pressure_level
+VALUES(201001, '120_90'),
+(201002, '135_80'),
+(201003, '119_81'),
+(201004, '120_80'),
+(201005, '135_80'),
+(201006, '130_80'),
+(201007, '130_80'),
+(201008, '130_80'),
+(201009, '130_80'),
+(201010, '130_80'),
+(201011, '130_80'),
+(201012, '120_80'),
+(201013, '130_80'),
+(201014, '125_80'),
+(201015, '130_80'),
+(201016, '130_80'),
+(201017, '130_80'),
+(201018, '130_80'),
+(201019, '130_80'),
+(201020, '120_80');
+
+
+/*Displaying the basic details of patient like blood_pressure, blood_sugar, weight, height, gender*/
+CREATE OR REPLACE VIEW patient_bs_bp_level AS
+    SELECT 
+        ph.patient_id, pd.patient_name, pd.height, pd.weight, pd.gender, ph.blood_sugar_level, bp.blood_pressure_level
+    FROM
+        Hospital_mgmt_db.Patients_Health_History ph
+            JOIN
+        Hospital_mgmt_db.patients_blood_pressure_level bp ON ph.patient_id = bp.patient_id
+			join
+		Hospital_mgmt_db.patient_details pd on ph.patient_id = pd.patient_id;
+        
+SELECt * from patient_bs_bp_level;
+
+/*Calculating the BMI*/
+DELIMITER $$
+USE Hospital_mgmt_db $$
+CREATE PROCEDURE BMI_calculation (IN p_id int)
+BEGIN
+SELECT patient_id, patient_name, cast(ROUND(weight / POWER(height/100, 2), 3) as decimal(10,2)) as BMI
+FROM
+Hospital_mgmt_db.patient_details
+where patient_id = p_id;
+END $$
+
+DELIMITER ;
+
+UPDATE Hospital_mgmt_db.patient_details
+SET weight = 70
+WHERE patient_id = 201001;
+
+DROP TABLE Hospital_mgmt_db.staff;
